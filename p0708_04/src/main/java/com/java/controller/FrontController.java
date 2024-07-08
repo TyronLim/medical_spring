5d39f5497b00c6635417abb9aa21351a7feb70e5
TyronLim <ltk6533@naver.com>
2024-07-08 18:10:45 +0900

    form data전송

---
+++ p0708_04/src/main/java/com/java/controller/FrontController.java
@@ -0,0 +1,89 @@
+package com.java.controller;
+
+import java.util.Arrays;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class FrontController {
+
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@RequestMapping("/member")
+	public String member() {
+		return "member";
+	}
+	
+	@RequestMapping("/doMember") // 회원가입
+	public ModelAndView doMember(HttpServletRequest request) {
+		String hobby[] = request.getParameterValues("hobby");
+		String hobbys="";
+		for(int i=0;i<hobby.length;i++) {
+			if(i==0) hobbys+=""+hobby[i];
+			else hobbys += ","+hobby[i];
+		}
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("id", request.getParameter("id"));
+		mv.addObject("pw", request.getParameter("pw"));
+		mv.addObject("name", request.getParameter("name"));
+		mv.addObject("phone", request.getParameter("phone"));
+		mv.addObject("gender", request.getParameter("gender"));
+		mv.addObject("hobby",hobbys);
+		mv.setViewName("doMember");
+		return mv;
+		
+	}
+	
+	@RequestMapping("/memUpdate") //회원정보 수정
+	public ModelAndView memUpdate(HttpServletRequest request) {
+		
+		String hobby[] = request.getParameterValues("hobby");
+		String hobbys="";
+		for(int i=0;i<hobby.length;i++) {
+			if(i==0) hobbys+=""+hobby[i];
+			else hobbys += ","+hobby[i];
+		}
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("id", request.getParameter("id"));
+		mv.addObject("pw", request.getParameter("pw"));
+		mv.addObject("name", request.getParameter("name"));
+		mv.addObject("phone", request.getParameter("phone"));
+		mv.addObject("gender", request.getParameter("gender"));
+		mv.addObject("hobby",hobbys);
+		mv.setViewName("doMember");
+		return mv;
+	}
+	
+	
+	
+	
+	
+//	@RequestMapping("/doMember")
+//	public String doMember(HttpServletRequest request, Model model) {
+//		String hobby[] = request.getParameterValues("hobby");
+//		model.addAttribute("id", request.getParameter("id"));
+//		model.addAttribute("pw", request.getParameter("pw"));
+//		model.addAttribute("name", request.getParameter("name"));
+//		model.addAttribute("phone", request.getParameter("phone"));
+//		model.addAttribute("gender", request.getParameter("gender"));
+//		model.addAttribute("hobby",Arrays.toString(hobby));
+//		return "doMember";
+//	}
+	
+	
+}
---
+++ p0708_04/src/main/webapp/WEB-INF/views/member.jsp
@@ -0,0 +1,40 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>정보입력</title>
+</head>
+<body>
+	<h2>정보 입력</h2>
+	<form action="doMember" name="frm" method="get">
+		<label>id</label>
+		<input type="text" name="id"><br>
+		
+		<label>pw</label>
+		<input type="password" name="pw"><br>
+		
+		<label>name</label>
+		<input type="test" name="name"><br>
+
+		<label>phone</label>
+		<input type="test" name="phone"><br>
+		
+		<input type="radio" name="gender" id= "male" value="male">남
+		<input type="radio" name="gender" id = "female" value="female">여<br>
+	
+		<label>취미</label><br>
+		<input type="checkbox" name="hobby" id="book" value="book">
+		<label for="book">독서</label>
+		<input type="checkbox" name="hobby" id="run" value="run">
+		<label for="run">조깅</label>
+		<input type="checkbox" name="hobby" id="fish" value="fish">
+		<label for="fish">낚시</label>
+		
+		<br>
+		<input type="submit" value="확인">
+	
+	</form>
+</body>
+</html>
