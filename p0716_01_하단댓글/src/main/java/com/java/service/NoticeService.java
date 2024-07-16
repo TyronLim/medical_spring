1990cebe2c17fa7ac9997209623482c834bb1375
TyronLim <ltk6533@naver.com>
2024-07-16 18:20:15 +0900

    댓글

---
+++ p0716_01_하단댓글/src/main/java/com/java/service/NoticeService.java
@@ -0,0 +1,19 @@
+package com.java.service;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import com.java.dto.CommentBDto;
+import com.java.dto.NoticeDto;
+
+public interface NoticeService {
+
+	ArrayList<NoticeDto> selectAll();
+
+	Map<String, Object> selectOne(NoticeDto noticeDto);
+
+	CommentBDto insertCommentB(CommentBDto commentBDto);	// 하단댓글 저장, 1개 가져오기
+
+	void deleteCommentB(CommentBDto commentBDto);	// 하단댓글 삭제
+
+}
